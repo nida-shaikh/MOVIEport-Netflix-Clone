b404250467f6da2cbe78b6320a1b99ab6bb4bf5f
Nida Shaikh <nida@example.com>
2026-05-09 19:35:07 +0500

    Fixed API URL to https for Vercel deployment

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://www.omdbapi.com/",
+  baseURL: "https://www.omdbapi.com/", // ✅ YAHAN 's' ADD KARO
   params: {
     apikey: "cb2b99f5", 
-    type: "series", // <-- Ye add kiya hai, ab sirf TV Series aayengi
+    type: "series",
   },
 });
 
